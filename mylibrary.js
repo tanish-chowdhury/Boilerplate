d5e03dc2bad740b0118a5d722b628213502c0542
tanish-chowdhury <73286641+tanish-chowdhury@users.noreply.github.com>
2020-11-10 15:11:55 +0530

    Add files via upload

---
+++ mylibrary.js
@@ -0,0 +1,10 @@
+function isTouch (a,b){
+    if (a.x-b.x < a.width/2+b.width/2 && b.x-a.x < a.width/2+b.width/2 && a.y-b.y<a.hieght/2+b.hieght&& b.y-a.y<a.hieght/2+b.hieght/2){
+     return true;
+
+    
+      }
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  /*movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;*/
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x=mouseX;
+  movingRect.y=mouseY;
+  
+if(isTouch (fixedRect,movingRect)){
+
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+}
+  
+  drawSprites();
+}
+
+
+- [ ] 
